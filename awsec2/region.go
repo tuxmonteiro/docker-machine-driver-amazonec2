4b48ae48f3ad4a27ddc487b588b311e8ea3e0049
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2019-09-15 15:27:52 -0300

    update aws region AMI (using 18.04 LTS instead 16.04 LTS)

--- awsec2/region.go
+++ awsec2/region.go
@@ -24,28 +24,32 @@ type region struct {
 	AmiId string
 }
 
-// Ubuntu 16.04 LTS 20180228.1 hvm:ebs-ssd (amd64)
+// Ubuntu 18.04 LTS 20190911 hvm:ebs-ssd (amd64)
 // See https://cloud-images.ubuntu.com/locator/ec2/
 var regionDetails map[string]*region = map[string]*region{
-	"ap-northeast-1":  {"ami-bcb7f6da"},
-	"ap-northeast-2":  {"ami-5073de3e"},
-	"ap-southeast-1":  {"ami-41e4af3d"},
-	"ap-southeast-2":  {"ami-c1498fa3"},
-	"ap-south-1":      {"ami-1083dc7f"},
-	"ca-central-1":    {"ami-8d9e19e9"},
-	"cn-north-1":      {"ami-cc4499a1"}, // Note: this is 20180126
-	"cn-northwest-1":  {"ami-fd0e1a9f"}, // Note: this is 20180126
-	"eu-north-1":      {"ami-017ff17f"},
-	"eu-central-1":    {"ami-bc4925d3"},
-	"eu-west-1":       {"ami-0b541372"},
-	"eu-west-2":       {"ami-ff46a298"},
-	"eu-west-3":       {"ami-9465d3e9"},
-	"sa-east-1":       {"ami-b5501bd9"},
-	"us-east-1":       {"ami-927185ef"},
-	"us-east-2":       {"ami-b9daeddc"},
-	"us-west-1":       {"ami-264c4646"},
-	"us-west-2":       {"ami-78a22900"},
-	"us-gov-west-1":   {"ami-2561ea44"},
+	"ap-east-1":       {"ami-d8cdb6a9"},
+	"ap-northeast-1":  {"ami-0193f3ddafc2d8921"},
+	"ap-south-1":      {"ami-03d48fdf7c142da15"},
+	"ap-southeast-1":  {"ami-0f8c65cdcc35d0a72"},
+	"ca-central-1":    {"ami-0e76f0489f3cca311"},
+	"eu-central-1":    {"ami-0fd6d676e2e90dc7b"},
+	"eu-north-1":      {"ami-dbc04aa5"},
+	"eu-west-1":       {"ami-08f053fa3d25478f4"},
+	"me-south-1":      {"ami-07ca5b987e63907ba"},
+	"sa-east-1":       {"ami-0017950c58e65a5a4"},
+	"us-east-1":       {"ami-024582e76075564db"},
+	"us-west-1":       {"ami-0c6eca62e6c7dacff"},
+	"cn-north-1":      {"ami-01993b4213b4bffb5"}, // Note: this is 20190627.1
+	"cn-northwest-1":  {"ami-01d4e30d4d4952d0f"}, // Note: this is 20190627.1
+	"us-gov-west-1":   {"ami-60501e01"},          // Note: this is 20190814
+	"us-gov-east-1":   {"ami-cce606bd"},          // Note: this is 20190814
+	"ap-northeast-2":  {"ami-0b0d9551c4775f4e4"},
+	"ap-southeast-2":  {"ami-056280928f86f80a8"},
+	"eu-west-2":       {"ami-012d95ddd53c98e0e"},
+	"us-east-2":       {"ami-090a9429be63ca087"},
+	"us-west-2":       {"ami-09c08e9cd8d9c1b93"},
+	"ap-northeast-3":  {"ami-08801b4823195185b"},
+	"eu-west-3":       {"ami-0001c7938b8e89312"},
 	"custom-endpoint": {""},
 }
 
